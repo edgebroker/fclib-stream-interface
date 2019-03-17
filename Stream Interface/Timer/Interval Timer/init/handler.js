682ea43bb05297eae13509fd7c955d57783a6585
iitsoftware <am@iit.de>
2019-03-17 13:32:57 +0100

    Throw exception if all values are 0

--- Stream Interface/Timer/Interval Timer/init/handler.js
+++ Stream Interface/Timer/Interval Timer/init/handler.js
@@ -1,5 +1,13 @@
 function handler() {
     var self = this;
+    
+    if (this.props["milliseconds"] === 0 &&
+        this.props["seconds"] === 0 &&
+        this.props["minutes"] === 0 &&
+        this.props["hours"] === 0 &&
+        this.props["days"] === 0)
+        throw "At least one timer property value must be > 0!";
+
     stream.create().timer(this.compid).interval()
         .milliseconds(this.props["milliseconds"])
         .seconds(this.props["seconds"])
