79d233afc0e5a06ef35215beef0d0320ef29c82f
Mike Wu <mail@wumike.com>
2019-04-11 21:08:22 +0700

    Define reference output in init for Body to JSON.

--- Stream Interface/Message/Body/Body to JSON/links/input/In/handler.js
+++ Stream Interface/Message/Body/Body to JSON/links/input/In/handler.js
@@ -1,21 +1,16 @@
 function handler(In) {
-     var json = JsonFromMessage(In);
-     this.executeOutputLink("Out", json);
-     this.setOutputReference("Message", getJson);
+    this.json = JsonFromMessage(In);
 
-     function JsonFromMessage(In) {
-         var result = {};
-         In.properties().forEach(function(prop) {
-             var propName = prop.name();
-             var value = prop.value().toObject();
-             result[propName]  = value;
-         });
-         return result;
-     }
+    this.executeOutputLink("Out", this.json);
 
-     function getJson(){
-         return json;
-     }
-
+    function JsonFromMessage(In) {
+        var result = {};
+        In.properties().forEach(function (prop) {
+            var propName = prop.name();
+            var value = prop.value().toObject();
+            result[propName] = value;
+        });
+        return result;
+    }
 
 }
--- Stream Interface/Message/Body/JSON to Message/links/input/In/handler.js
+++ Stream Interface/Message/Body/JSON to Message/links/input/In/handler.js
@@ -1,15 +1,14 @@
 function handler(In) {
 
     var outMsg = stream.create().message().message();
-    stream.log().info(JSON.stringify(this.getInputReference("Message")));
     var source = this.getInputReference("Message")();
-    for(var key in source) {
+    for (var key in source) {
         outMsg.property(key).set(source[key]);
     }
-
-    this.executeOutputLink("Out", outMsg);
 
     outMsg.properties().forEach(function (prop) {
         stream.log().info(prop.name() + ": " + prop.value().toObject());
     });
+
+    this.executeOutputLink("Out", outMsg);
 }
