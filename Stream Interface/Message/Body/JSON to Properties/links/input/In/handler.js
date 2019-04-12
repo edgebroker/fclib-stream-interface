ffcf87f20966be9d95ee2a48301c29693c1a2985
Mike Wu <mail@wumike.com>
2019-04-12 20:42:52 +0700

    Set properties on received message on 'JSON to Properties'.

--- Stream Interface/Message/Body/JSON to Properties/links/input/In/handler.js
+++ Stream Interface/Message/Body/JSON to Properties/links/input/In/handler.js
@@ -3,13 +3,12 @@ function handler(In) {
     var fields = this.props["fields"];
     var properties = this.props["properties"];
 
-    var outMsg = stream.create().message().message();
     var source = this.getInputReference("Message")();
 
     fields.forEach(function(field, index) {
         var name = properties[index];
-        outMsg.property(name).set(source[field]);
+        In.property(name).set(source[field]);
     });
 
-    this.executeOutputLink("Out", outMsg);
+    this.executeOutputLink("Out", In);
 }
