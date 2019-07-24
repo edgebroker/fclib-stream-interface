785b5e1a8b562a0974de30e47c2f307a4a8bb956
iitsoftware <am@iit.de>
2019-07-24 16:10:36 +0200

    assert replyto

--- Stream Interface/Output/Reply Output/links/input/In/handler.js
+++ Stream Interface/Output/Reply Output/links/input/In/handler.js
@@ -1,6 +1,8 @@
 function handler(In) {
     var outMsg = stream.create().message().copyMessage(In);
     outMsg.getImpl().clearSwiftMQAllProps();
+    if (In.replyTo() === null)
+        throw "Missing 'replyTo' in incoming message!";
     stream.create().output(null).forAddress(outMsg.replyTo()).send(outMsg).close();
     this.executeOutputLink("Out", outMsg);
 }
