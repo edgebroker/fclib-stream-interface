424479ba7cfe20d65debda4ed60c197cfb1cb9b2
iitsoftware <am@iit.de>
2019-09-10 11:48:22 +0200

    handle arrays that contains plain values (non JSON objects)

--- Stream Interface/Message/Body/JSON Array to Messages/links/input/In/handler.js
+++ Stream Interface/Message/Body/JSON Array to Messages/links/input/In/handler.js
@@ -6,15 +6,23 @@ function handler(In) {
 
     if (body) {
         var array = JSON.parse(body);
+        if (!Array.isArray(array))
+            throw "Body content is not an array!";
         array.forEach(function (item) {
             var resultMessage = stream.create().message().textMessage();
 
-            Object.keys(item).forEach(function (key) {
-                var propName = key.replace('-', '_').replace(/[!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/, '')
-                var value = item[key];
-                resultMessage.property(propName).set(value);
-            });
-
+            if (typeof item === "object") {
+                Object.keys(item).forEach(function (key) {
+                    var propName = key.replace('-', '_').replace(/[!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/, '');
+                    var value = item[key];
+                    resultMessage.property(propName).set(value);
+                });
+            } else {
+                if (props["propname"]) {
+                    resultMessage.property(props["propname"]).set(item);
+                } else
+                    throw "Property 'propname' must be set."
+            }
             self.executeOutputLink("ForEach", resultMessage);
         });
     }
