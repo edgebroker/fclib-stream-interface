6fc6be67c16c9788861736e7eadfe7243fe08a85
iitsoftware <am@iit.de>
2019-09-25 18:28:04 +0200

    bug fix dynamic outputs

--- Stream Interface/Output/Dynamic Topic Output/links/input/In/handler.js
+++ Stream Interface/Output/Dynamic Topic Output/links/input/In/handler.js
@@ -1,6 +1,6 @@
 function handler(In) {
     this.assertProperty(In, this.props["topicnameprop"]);
-    var topicName = this.props["topicnameprop"];
+    var topicName = In.property(this.props["topicnameprop"]).value().toString();
     stream.create().output(topicName).topic();
     stream.output(topicName).send(In);
     stream.output(topicName).close();
