5be07f0fe031a535a52d691f700876a95fed261d
iitsoftware <am@iit.de>
2019-11-11 14:06:39 +0100

    add delay, occupancy

--- Stream Interface/Message/Body/GTFS Decode/links/input/In/handler.js
+++ Stream Interface/Message/Body/GTFS Decode/links/input/In/handler.js
@@ -10,17 +10,37 @@ function handler(In) {
         var out = stream.create().message().textMessage();
         out.copyProperties(In);
         var entity = entities.get(i);
-        var json = {
-            routeid: entity.getVehicle().getTrip().getRouteId(),
-            long: entity.getVehicle().getPosition().getLongitude(),
-            lat: entity.getVehicle().getPosition().getLatitude(),
-            timestamp: entity.getVehicle().getTimestamp()
-        };
-        out.property("routeid").set(json.routeid);
-        out.property("long").set(json.long);
-        out.property("lat").set(json.lat);
-        out.property("timestamp").set(json.timestamp);
-        out.body(JSON.stringify(json));
-        this.executeOutputLink("Out", out);
+        if (entity.getVehicle().getTrip().hasRouteId()) {
+            var delay = 0;
+            var occupancy = "GREEN";
+            if (entity.hasTripUpdate() && entity.getTripUpdate().hasDelay())
+                delay = entity.getTripUpdate().hasDelay();
+            if (entity.getVehicle().hasOccupancyStatus()){
+                switch (entity.getVehicle().hasOccupancyStatus()){
+                    case 2:
+                    case 3:
+                        occupancy = "YELLOW";
+                    case 4:
+                    case 5:
+                        occupancy = "RED";
+                }
+            }
+            var json = {
+                routeid: entity.getVehicle().getTrip().getRouteId(),
+                long: entity.getVehicle().getPosition().getLongitude(),
+                lat: entity.getVehicle().getPosition().getLatitude(),
+                timestamp: entity.getVehicle().getTimestamp(),
+                delay: delay,
+                occupancy: occupancy
+            };
+            out.property("routeid").set(json.routeid);
+            out.property("long").set(json.long);
+            out.property("lat").set(json.lat);
+            out.property("delay").set(json.delay);
+            out.property("occupancy").set(json.occupancy);
+            out.property("timestamp").set(json.timestamp);
+            out.body(JSON.stringify(json));
+            this.executeOutputLink("Out", out);
+        }
     }
 }
