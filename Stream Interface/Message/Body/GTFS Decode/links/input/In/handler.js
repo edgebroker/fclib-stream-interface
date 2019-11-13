eada8cd2949616d3a94780d761e10f7c6c802a00
iitsoftware <am@iit.de>
2019-11-13 13:19:39 +0100

    json changed

--- Stream Interface/Message/Body/GTFS Decode/links/input/In/handler.js
+++ Stream Interface/Message/Body/GTFS Decode/links/input/In/handler.js
@@ -10,30 +10,24 @@ function handler(In) {
         var out = stream.create().message().textMessage();
         out.copyProperties(In);
         var entity = entities.get(i);
-        var delay = entity.getTripUpdate().getDelay();
-        var occupancy = "GREEN";
-        switch (entity.getVehicle().getOccupancyStatus()) {
-            case 2:
-            case 3:
-                occupancy = "YELLOW";
-            case 4:
-            case 5:
-                occupancy = "RED";
-        }
         var json = {
-            routeid: entity.getVehicle().getVehicle().getId(),
+            id: entity.getVehicle().getVehicle().getId(),
+            label: entity.getVehicle().getVehicle().getId(),
             long: entity.getVehicle().getPosition().getLongitude(),
             lat: entity.getVehicle().getPosition().getLatitude(),
-            timestamp: entity.getVehicle().getTimestamp(),
-            delay: delay,
-            occupancy: occupancy
+            timestamp: entity.getVehicle().getTimestamp()
         };
-        out.property("routeid").set(json.routeid);
+        if (entity.getVehicle().getVehicle().hasLabel())
+            json.label = entity.getVehicle().getVehicle().getLabel();
+        if (entity.getVehicle().getPosition().hasSpeed())
+            json.speed = entity.getVehicle().getPosition().getSpeed();
+        out.property("id").set(json.id);
+        out.property("label").set(json.label);
         out.property("long").set(json.long);
         out.property("lat").set(json.lat);
-        out.property("delay").set(json.delay);
-        out.property("occupancy").set(json.occupancy);
         out.property("timestamp").set(json.timestamp);
+        if (json.speed)
+            out.property("speed").set(json.speed);
         out.body(JSON.stringify(json));
         this.executeOutputLink("Out", out);
     }
