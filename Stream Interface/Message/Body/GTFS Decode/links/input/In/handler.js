05609c64e74a36e3c868383b6a8d0e1a52139aa7
iitsoftware <am@iit.de>
2019-11-14 16:52:48 +0100

    bug fixes

--- Stream Interface/Message/Body/GTFS Decode/links/input/In/handler.js
+++ Stream Interface/Message/Body/GTFS Decode/links/input/In/handler.js
@@ -7,28 +7,18 @@ function handler(In) {
     var feedMessage = FEEDMESSAGE.parseFrom(In.body());
     var entities = feedMessage.getEntityList();
     for (var i = 0; i < entities.size(); i++) {
-        var out = stream.create().message().textMessage();
+        var out = stream.create().message().message();
         out.copyProperties(In);
         var entity = entities.get(i);
-        var json = {
-            id: entity.getVehicle().getVehicle().getId(),
-            label: entity.getVehicle().getVehicle().getId(),
-            long: entity.getVehicle().getPosition().getLongitude(),
-            lat: entity.getVehicle().getPosition().getLatitude(),
-            timestamp: entity.getVehicle().getTimestamp()
-        };
+        out.property("id").set(entity.getVehicle().getVehicle().getId());
+        out.property("label").set(entity.getVehicle().getVehicle().getId());
         if (entity.getVehicle().getVehicle().hasLabel())
-            json.label = entity.getVehicle().getVehicle().getLabel();
+            out.property("label").set(entity.getVehicle().getVehicle().getLabel());
+        out.property("long").set(entity.getVehicle().getPosition().getLongitude());
+        out.property("lat").set(entity.getVehicle().getPosition().getLatitude());
+        out.property("timestamp").set(entity.getVehicle().getTimestamp());
         if (entity.getVehicle().getPosition().hasSpeed())
-            json.speed = entity.getVehicle().getPosition().getSpeed();
-        out.property("id").set(json.id);
-        out.property("label").set(json.label);
-        out.property("long").set(json.long);
-        out.property("lat").set(json.lat);
-        out.property("timestamp").set(json.timestamp);
-        if (json.speed)
-            out.property("speed").set(json.speed);
-        out.body(JSON.stringify(json));
+            out.property("speed").set(entity.getVehicle().getPosition().getSpeed());
         this.executeOutputLink("Out", out);
     }
 }
