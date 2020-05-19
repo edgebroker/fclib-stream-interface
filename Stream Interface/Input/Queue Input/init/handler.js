5bae634ae7b5f53e3008ff08580843ae5f892167
iitsoftware <am@iit.de>
2020-05-19 13:31:43 +0200

    - autocreate for queue inputs / outputs - flag as persistent for queue outputs

--- Stream Interface/Input/Queue Input/init/handler.js
+++ Stream Interface/Input/Queue Input/init/handler.js
@@ -1,5 +1,12 @@
 function handler() {
     var self = this;
+    if (this.props["autocreate"]) {
+        stream.cli()
+            .exceptionOff()
+            .execute("cc sys$queuemanager/queues")
+            .execute("new "+this.props["queuename"])
+            .execute("save");
+    }
     if (this.props["wiretap"] === true) {
         if (this.props["selector"]) {
             stream.create().input(this.props["queuename"])
--- Stream Interface/Output/Dynamic Queue Output/links/input/In/handler.js
+++ Stream Interface/Output/Dynamic Queue Output/links/input/In/handler.js
@@ -1,8 +1,13 @@
 function handler(In) {
     this.assertProperty(In, this.props["queuenameprop"]);
     var queueName = In.property(this.props["queuenameprop"]).value().toString();
+    this.createIfNecessary(queueName);
     if (stream.output(queueName) === null)
         stream.create().output(queueName).queue();
+    if (this.props["persistent"])
+        In.persistent();
+    else
+        In.nonpersistent();
     stream.output(queueName).send(In);
     this.executeOutputLink("Out", In);
 }
